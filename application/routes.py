951f773e5b1bd7fa4082bd26eea2c2e1fa357d1f
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-05-20 14:42:56 +0100

    added cases route and json data

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,13 @@
 import json
 
 from application import app
-from flask import render_template
-
 
 @app.route('/', methods=["GET"])
 def index():
     return 'Cases API'
+
+@app.route('/cases', methods=["GET"])
+def cases():
+    json_data=open('application/static/data/cases.json', "r")
+    data = json.load(json_data)
+    return json.dumps(data)
