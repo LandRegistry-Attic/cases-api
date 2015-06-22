82397b52c0d8bd7141010b459b09d3166202abff
LlamaComedian <LlamaComedian@github.com>
2015-06-22 14:52:02 +0100

    added code to call the titles API to validate the title

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 import json
 
 from application import app
-from .utils import add_to_daylist
+from .utils import add_to_daylist, validate_title
 
 from flask import request, Response
 
@@ -20,40 +20,39 @@ def getCases():
         case_data = request.get_json()
         title_number = case_data["titleNumber"]
 
-        ##title_validation_code = validate_title(title_number)
-        ##if title_validation_code == 1:
+        title_validation_code = validate_title(title_number)
+        if title_validation_code == 1:
 
-        application_reference = add_to_daylist(title_number)
+            application_reference = add_to_daylist(title_number)
 
-        #Get current case list
-        jsonFile=open('application/static/data/cases.json')
-        case_list = json.load(jsonFile)
+            #Get current case list
+            jsonFile=open('application/static/data/cases.json')
+            case_list = json.load(jsonFile)
 
-        #Create a new case from the case_data received
-        case = {}
-        case["titleNumber"] = title_number
-        case["applicationReference"] = application_reference
-        case["dateReceived"] = case_data["dateReceived"]
-        case["mortgageDate"] = case_data["mortgageDate"]
-        case["lender"] = case_data["lender"]
-        case["submissionRef"] = case_data["submissionRef"]
-        case["keyNumber"] = case_data["keyNumber"]
-        case["amountPaid"] = case_data["amountPaid"]
-        case["borrower"] = case_data["borrower"]
-        case["propertyDetails"] = case_data["propertyDetails"]
-        case["emdref"] = case_data["emdref"]
+            #Create a new case from the case_data received
+            case = {}
+            case["titleNumber"] = title_number
+            case["applicationReference"] = application_reference
+            case["dateReceived"] = case_data["dateReceived"]
+            case["mortgageDate"] = case_data["mortgageDate"]
+            case["lender"] = case_data["lender"]
+            case["submissionRef"] = case_data["submissionRef"]
+            case["keyNumber"] = case_data["keyNumber"]
+            case["amountPaid"] = case_data["amountPaid"]
+            case["borrower"] = case_data["borrower"]
+            case["propertyDetails"] = case_data["propertyDetails"]
+            case["emdref"] = case_data["emdref"]
 
-        case_list["cases"].append(case)
+            case_list["cases"].append(case)
 
-        jsonFile=open('application/static/data/cases.json', "w")
-        jsonFile.write(json.dumps(case_list, sort_keys=True, indent=4, separators=(',', ': ')))
+            jsonFile=open('application/static/data/cases.json', "w")
+            jsonFile.write(json.dumps(case_list, sort_keys=True, indent=4, separators=(',', ': ')))
+
+        else:
+            application_reference = ""
 
         #Build response
-        resp = Response('{"submissionRef" : "' + case["submissionRef"]+ '", "applicationReference" : "' + case["applicationReference"]+ '"}', status=200, mimetype='application/json')
-
-        ##else:
-        ##    resp = "title invalid"
-
+        resp = Response('{"submissionRef" : "' + case_data["submissionRef"] + '", "applicationReference" : "' + application_reference + '", "TitleValidationCode" : "' + title_validation_code + '"}', status=200, mimetype='application/json')
         return resp
 
 
--- application/utils.py
+++ application/utils.py
@@ -1,18 +1,17 @@
 import json
 import requests
+import os
 
 def add_to_daylist(title_number):
-    #with open('application/static/data/reference.txt',"r+") as file:
-    #    ref_number = file.read()
-    #    ref_int = int(ref_number) + 1
-    #    ref_number = str(ref_int)
-    #    file.seek(0)
-    #    file.write(ref_number)
-
-    #reference = "AB" + ref_number
-
     response = requests.post('http://localhost:8888/cases/' + title_number)
     data = response.json()
     reference = data["abr"]
 
     return reference
+
+def validate_title(title_number):
+    response = requests.get(os.getenv("TITLES_API_URL","http://localhost:5005/validate/") + title_number)
+    data = response.json()
+    validation_result = data["validation_result"]
+
+    return validation_result
